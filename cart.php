14bb327a432a32501b14d86d887fafee51929cc8
Andrei Ghiorghe <[andrei.bontu@5psolutions.eu]>
2020-06-29 17:07:19 +0300

    "{#5} {cart.php} {http://pm.5psolutions.net/public/index.php?path_info=projects%2Ftraining-andrei-ghiorghe%2Ftasks%2F5} {This commit deleted some empty lines, and rearanged the code.}"

--- cart.php
+++ cart.php
@@ -50,71 +50,70 @@ $items = extract_products($connection, $my_cart, INSIDE_CART);
 
 ob_start();
 ?>
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <link rel="stylesheet" type="text/css" href="http://localhost/Test/frontend/style.css">
-        <meta charset="UTF-8">
-        <title> <?= translate('Cart Page') ?> </title>
-    </head>
-    <body>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="http://localhost/Test/frontend/style.css">
+    <meta charset="UTF-8">
+    <title> <?= translate('Cart Page') ?> </title>
+</head>
+<body>
 
-    <table id="content_table">
-        <tbody>
-        <?php for ($i = 0; $i < count($items); $i++): ?>
-            <?php ?>
-            <?php if (isset($my_cart[intval($items[$i]['id'])])): ?>
-                <tr class="element_of_table">
-                    <td>
-                        <img class="phone_image" src="<?= $items[$i]['image_path'] ?>">
-                    </td>
-                    <td>
-                        <?= $items[$i]['title'] ?><br>
-                        <?= $items[$i]['description'] ?><br>
-                        <?= $items[$i]['price'] ?> <?= translate('lei') ?><br>
-                    </td>
-                    <td>
-                        <form method="post" action="cart.php">
-                            <input type="hidden" name="id_product" value="<?= $items[$i]['id'] ?>">
-                            <button type="submit" class="link_button"> <?= translate('Remove') ?> </button>
-                        </form>
-                    </td>
-                </tr>
-                <br>
-            <?php endif; ?>
-        <?php endfor; ?>
-        <form action="cart.php" method="POST">
-            <tr>
+<table id="content_table">
+    <tbody>
+    <?php for ($i = 0; $i < count($items); $i++): ?>
+        <?php if (isset($my_cart[intval($items[$i]['id'])])): ?>
+            <tr class="element_of_table">
                 <td>
-                    <input class="input_type" type="text" name="name_field" placeholder="<?= translate('Name') ?>"
-                           value="<?= translate($name_field) ?>">
-                    <span class="error_field">* <?= translate($name_field_error) ?></span>
+                    <img class="phone_image" src="<?= $items[$i]['image_path'] ?>">
+                </td>
+                <td>
+                    <?= $items[$i]['title'] ?><br>
+                    <?= $items[$i]['description'] ?><br>
+                    <?= $items[$i]['price'] ?> <?= translate('lei') ?><br>
+                </td>
+                <td>
+                    <form method="post" action="cart.php">
+                        <input type="hidden" name="id_product" value="<?= $items[$i]['id'] ?>">
+                        <button type="submit" class="link_button"> <?= translate('Remove') ?> </button>
+                    </form>
                 </td>
             </tr>
-            <tr>
-                <td>
-                    <input class="input_type" type="text" name="address_field"
-                           placeholder="<?= translate('Contact deatails') ?>" value="<?= translate($address_field) ?>">
-                    <span class="error_field">* <?= translate($address_field_error) ?></span>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                <textarea id="comments_section" class="input_type" type="text" name="comments_field"
-                          placeholder="<?= translate('Comments') ?>"><?= translate($comments_field) ?></textarea>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <a href="index.php"><?= translate('Go to index') ?></a>
-                    <input type="submit" name="submit_button" value="Checkout">
-                </td>
-            </tr>
-        </form>
-        </tbody>
-    </table>
-    </body>
-    </html>
+            <br>
+        <?php endif; ?>
+    <?php endfor; ?>
+    <form action="cart.php" method="POST">
+        <tr>
+            <td>
+                <input class="input_type" type="text" name="name_field" placeholder="<?= translate('Name') ?>"
+                       value="<?= translate($name_field) ?>">
+                <span class="error_field">* <?= translate($name_field_error) ?></span>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input class="input_type" type="text" name="address_field"
+                       placeholder="<?= translate('Contact deatails') ?>" value="<?= translate($address_field) ?>">
+                <span class="error_field">* <?= translate($address_field_error) ?></span>
+            </td>
+        </tr>
+        <tr>
+            <td>
+            <textarea id="comments_section" class="input_type" type="text" name="comments_field"
+                      placeholder="<?= translate('Comments') ?>"><?= translate($comments_field) ?></textarea>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <a href="index.php"><?= translate('Go to index') ?></a>
+                <input type="submit" name="submit_button" value="Checkout">
+            </td>
+        </tr>
+    </form>
+    </tbody>
+</table>
+</body>
+</html>
 <?php
 if ($send_email) {
     $html_page = ob_get_clean();
